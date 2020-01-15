a470fc6e376c318f7a17333c017a44e805516e67
chenxiaoli <39185753@qq.com>
2020-01-15 11:43:41 +0800

    none

--- utils/block.py
+++ utils/block.py
@@ -7,10 +7,10 @@ def parse_transaction(transaction):
     trades = []
     from_address = transaction.get("from")
     to_address = transaction.get("to")
-    value = transaction["value"] # 这里注意溢出
+    value = transaction["value"]  # 这里注意溢出
     block_number = transaction.get("blockNumber")
     transaction_hash = w3.toHex(transaction.get("hash"))
-    if from_address and to_address and value:
+    if from_address and to_address and value > 0:
         trades.append(
             {"from": from_address, "to": to_address, "value": value, "symbol": "eth",
              "block_number": block_number,
@@ -25,10 +25,13 @@ def parse_transaction(transaction):
             from_address = str(topics[1]).lower()
             to_address = str(topics[2]).lower()
             data = Web3.toInt(logs.data)  # 这里可能会出现异常,溢出
-            trades.append(
-                {"from": from_address, "to": to_address, "value": data, "contract_address": str(contract_address).lower(),
-                 "block_number": block_number, "transaction_hash": transaction_hash})
+            if data > 0:
+                trades.append(
+                    {"from": from_address, "to": to_address, "value": data,
+                     "contract_address": str(contract_address).lower(),
+                     "block_number": block_number, "transaction_hash": transaction_hash})
     return trades
+
 
 def parse_transaction_receipt(transaction_receipt):
     w3 = get_web3_instance()
@@ -36,26 +39,27 @@ def parse_transaction_receipt(transaction_receipt):
 
     block_number = transaction_receipt.get("blockNumber")
     transaction_hash = w3.toHex(transaction_receipt.get("transactionHash"))
-    if transaction_receipt.get("status")!=1:
+    if transaction_receipt.get("status") != 1:
         return []
     logs = transaction_receipt.get("logs", None)
     for log in logs:
-                     # 智能合约(erc20) logs中，topics[0]都是事件的keccka的hash结果；topics[1]topics[2]分别是from和to；address是合约地址；data是交易额；
-            contract_address = log.get("address", None)
-            topics = log.get("topics", None)
-            data = log.get("data", 0)
-            if contract_address and len(topics)==3 and data:
-                from_address = topics[1]
-                to_address = topics[2]
-                from_address=Web3.toHex(from_address[-20:])
-                to_address=Web3.toHex(to_address[-20:])
+        # 智能合约(erc20) logs中，topics[0]都是事件的keccka的hash结果；topics[1]topics[2]分别是from和to；address是合约地址；data是交易额；
+        contract_address = log.get("address", None)
+        topics = log.get("topics", None)
+        data = log.get("data", 0)
+        if contract_address and len(topics) == 3 and data:
+            from_address = topics[1]
+            to_address = topics[2]
+            from_address = Web3.toHex(from_address[-20:])
+            to_address = Web3.toHex(to_address[-20:])
 
-                try:
-                    data = Web3.toInt(hexstr=log.data)  # 这里可能会出现异常,溢出
+            try:
+                data = Web3.toInt(hexstr=log.data)  # 这里可能会出现异常,溢出
+                if data>0:
                     trades.append(
-                        {"from": str(from_address).lower(), "to": str(to_address).lower(), "value": data, "contract_address": str(contract_address).lower(),
+                        {"from": str(from_address).lower(), "to": str(to_address).lower(), "value": data,
+                         "contract_address": str(contract_address).lower(),
                          "block_number": block_number, "transaction_hash": transaction_hash})
-                except ValueError:
-                    pass
+            except ValueError:
+                pass
     return trades
-
