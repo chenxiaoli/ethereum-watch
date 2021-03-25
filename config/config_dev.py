f08c1245a927289da5b98441efb5249856dc3fc7
Your Name <you@example.com>
2021-03-25 23:23:58 +0800

    fixed

--- config/config_dev.py
+++ config/config_dev.py
@@ -18,7 +18,7 @@ configs = {
         "new_eth_block_arrived_topic": "new_eth_block_arrived_topic",  # 新区块产生
         "eth_block_number_queue": "eth_block_number_queue",  # 区块信息解析
         "eth_transaction_queue":"eth_transaction_queue",  # 事务解析解析
-        "new_eth_trades_queue": "new_eth_trades_queue",  # 新交易
+        "new_ethereum_trade_exchange": "new_ethereum_trade_exchange"  # 新交易
         # "subscriptions_queue":["..."],#订阅队列
     },
     "web3_provider":{
